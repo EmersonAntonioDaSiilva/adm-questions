4a3c0a70871f7367be218152054b9285ae1a2f9f
caguiar <caguiar@magnasistemas.com.br>
2016-08-09 12:22:45 -0300

    Ajustes no serviço do retrieve and rank
    Ajustes no serviço do retrieve and rank

--- adm-questions-web/src/main/java/br/com/afirmanet/questions/manager/application/additional/ClusterSolrManager.java
+++ adm-questions-web/src/main/java/br/com/afirmanet/questions/manager/application/additional/ClusterSolrManager.java
@@ -3,8 +3,6 @@ package br.com.afirmanet.questions.manager.application.additional;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -26,10 +24,6 @@ public class ClusterSolrManager extends AbstractManager implements Serializable 
 	@ApplicationManaged
 	private EntityManager entityManager;
 	
-	private static String nomeCluster = "MAGNA_RH_TEST",
-						  nomeConfig = "CONF_RH_TEST",
-			 			  nomeColection = "COLLEC_RH_TEST"; 
-	
 	private ServiceRetrieveAndRank service;
 	private Cliente cliente;
 	
@@ -43,32 +37,7 @@ public class ClusterSolrManager extends AbstractManager implements Serializable 
 
 	public void btGeraCluster(){
 		
-		FacesContext context = FacesContext.getCurrentInstance();
-		
-		try{
-			String idCluster = "";
-			if(!service.existsSolrCluster()){
-				
-				idCluster = service.createCluster(nomeCluster, null);
-				service.uploadConfiguration(idCluster, nomeConfig);
-				service.createCollection(idCluster, nomeConfig, nomeColection);
-				service.indexDocumentAndCommit(idCluster, nomeColection);
-				
-				// Mensagem após cluster criado
-				String mensagem = "Cluster foi criado com sucesso. O identificador do cluster é: "+ idCluster;
-				context.addMessage("clusterMessage", new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, mensagem));
-			}
-			else{
-				// Mensagem de cluster criado
-				context.addMessage("clusterMessage", new FacesMessage(FacesMessage.SEVERITY_INFO, "Cluster já criado.", "Cluster já criado."));
-			}
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			
-			// Exibe mensagem de erro na tela
-			context.addMessage("clusterMessage", new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
-		}
+		System.out.println(service.getIdClusterSolr());
 	}
 	
 }
