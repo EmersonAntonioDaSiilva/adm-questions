745e3f8c383bcc56c330e91ca62670c635fd0aba
antonioJT <antonio.jat12@gmail.com>
2016-08-16 11:42:40 -0300

    Alteração

---
+++ adm-questions-doc/searsh-questions-classification.sql
@@ -0,0 +1,8 @@
+﻿select 	* 
+from 	classificacoes 
+where 	data_cadastro between  '2016-08-01 00:00:00.000' and '2016-08-31 23:59:59.000' 
+and 	data_classificacao is null
+and 	sentimento not in(1, 100, 200)
+	Order by resposta, confidence;
+
+
